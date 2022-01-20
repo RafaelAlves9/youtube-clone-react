6a2d4f84454a62349bba578d6a2e399cc6a771e6
Rafael Alves <hrafael.alves9@gmail.com>
2022-01-20 13:47:12 -0300

    up

--- src/main/Login.jsx
+++ src/main/Login.jsx
@@ -1,17 +1,15 @@
 import "./All.css"
 import "./Login.css"
 
+import TittleLogin from "../components/login/TittleLogin"
+import InputLogin from "../components/login/InputLogin"
+import SendLogin from "../components/login/SendLogin"
+
 export default () => 
     <div className="container-login">
         <div className="content-login">
-            <div>
-                <img src=""/>
-                <h3>Fazer Login</h3>
-                <h4>Prosseguir no YouTube</h4>
-            </div>
-            <div>
-                <input type="email" placeholder="E-mail ou telefone"/>
-            </div>
-            <div></div>
+            <TittleLogin />
+            <InputLogin />
+            <SendLogin />
         </div>
     </div>
