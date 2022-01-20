0804a706dc36186f57b245aba6895be43ca8f884
Rafael Alves <hrafael.alves9@gmail.com>
2022-01-19 22:23:39 -0300

    up

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 
-import Routes from './Routes.jsx'
+import Routes from './Routes.js'
 
 ReactDOM.render(
   
   <BrowserRouter>
 
     <Routes />
-    
+
   </BrowserRouter>
   ,
   document.getElementById('root')
