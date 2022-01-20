ee8b6f08117f19286fd5ea6493255a66af7f0bc8
Rafael Alves <hrafael.alves9@gmail.com>
2022-01-20 19:51:25 -0300

    up

--- src/main/Login.jsx
+++ src/main/Login.jsx
@@ -1,11 +1,16 @@
+import TopBar from "../components/top-bar/top-bar.jsx"
+import LeftBar from "../components/left-bar/Left-bar.jsx"
+
+import "../components/top-bar/top-bar.css"
+import "../components/left-bar/left-bar.css"
 import "./All.css"
-import "./Login.css"
 
 import TittleLogin from "../components/login/TittleLogin"
 import InputLogin from "../components/login/InputLogin"
 import SendLogin from "../components/login/SendLogin"
 
 export default () => 
+<<<<<<< HEAD
     <div className="container-login">
         <div className="content-login">
             <TittleLogin />
@@ -13,3 +18,9 @@ export default () =>
             <SendLogin />
         </div>
     </div>
+=======
+<div className="all-content">
+    <TopBar />
+    <LeftBar />
+</div>
+>>>>>>> parent of 99236952 (up)
