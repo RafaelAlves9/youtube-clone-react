3191204ad1ae61fab91030faf9b2df23677bb7ff
Rafael Alves <hrafael.alves9@gmail.com>
2022-01-21 09:44:00 -0300

    up

--- src/components/top-bar/components-top-bar/opicion-menu.jsx
+++ src/components/top-bar/components-top-bar/opicion-menu.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import MenuA from './cardapio.png'
 import MenuB from './menu-de-pontos.png'
 import Perfil from './perfil.png'
@@ -6,7 +8,7 @@ export default () =>
     <div className="opicion-menu">
         <span className='opicions'><img src={MenuB} alt=""/></span>
         <span className='opicions'><img src={MenuA} alt=""/></span>
-        <span className='login'><img src={Perfil} alt=""/>FAZER LOGIN</span>
+        <Link><span className='login'><img src={Perfil} alt=""/>FAZER LOGIN</span></Link>
 
         {/* login mobile */}
         <span className='login-mobile'><img src={Perfil} alt=""/></span>
