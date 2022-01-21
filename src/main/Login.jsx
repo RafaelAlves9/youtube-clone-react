66c7fb61414df16f53a125591d7b789e55890b50
Rafael Alves <hrafael.alves9@gmail.com>
2022-01-21 09:56:17 -0300

    up

--- src/main/Login.jsx
+++ src/main/Login.jsx
@@ -1,4 +1,15 @@
+import "./All.css"
 import "./Login.css"
 
+import Google from "./google_logo.png"
+
 export default () => 
-    
+    <div className="container-login">
+        <div className="content-login">
+            <div className="tittle-login">
+                <img src={Google}/>
+                <p className="tittle">Fazer Login</p>
+                <p className="sub-tittle">Prosseguir no YouTube</p>
+            </div>
+        </div>
+    </div>
